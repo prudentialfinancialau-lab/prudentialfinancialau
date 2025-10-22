853e673e5dd6746fc7195ecb5dc9adaae7cc1a3a
root <ucchash.education@gmail.com>
2025-10-22 05:34:10 +0000

    Rebuild TinaCMS admin files with new Client ID
    - Rebuilt admin panel with Client ID: 4283e962-a6ee-457f-b47a-23cc4a5ae779
    - Updated all admin JavaScript bundles to use new TinaCMS Cloud project
    - This fixes CORS errors from old Client ID in production

--- tina/__generated__/client.d.ts
+++ tina/__generated__/client.d.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/var/www/development/upwork/loan-web/tinacms-app/tina/__generated__/.cache/1761110983298', url: 'https://content.tinajs.io/1.6/content/4283e962-a6ee-457f-b47a-23cc4a5ae779/github/main', token: '160bbe26b1293605cd34528135c8dabdc91e31c2', queries,  });
+export const client = createClient({ cacheDir: '/var/www/development/upwork/loan-web/tinacms-app/tina/__generated__/.cache/1761111181013', url: 'https://content.tinajs.io/1.6/content/4283e962-a6ee-457f-b47a-23cc4a5ae779/github/main', token: '160bbe26b1293605cd34528135c8dabdc91e31c2', queries,  });
 export default client;
   
